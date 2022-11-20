179f7ab56672d6d41c68d5dbcf253cef536a00ac
RobertJAlvarez <robert.alvarez644@gmail.com>
2022-11-20 11:09:14 -0700

    A lot of changes for the set up

--- main.c
+++ main.c
@@ -5,7 +5,15 @@
 
 int main(void)
 {
-  print_sizeof_struct();
+  //print_sizeof_struct();
+
+/*
+  const char path[85] = "/Users/robertalvarez/OneDrive - University of Texas at El Paso/Fall_2022/CS_4375/HW3";
+  char **tokens = tokenize('/', path);
+  print_tokens(tokens);
+*/
+
+  //
 
   return 0;
 }
