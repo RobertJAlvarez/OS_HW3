21e141c430b66d30ef48976bcf508bb24504280a
RobertJAlvarez <robert.alvarez644@gmail.com>
2022-12-07 15:47:52 -0700

    Small changes

--- main.c
+++ main.c
@@ -7,8 +7,6 @@
 
 int main(void)
 {
-  //printf("[%d %d %d %d:%d:%d]\n", t.day, t.month+1, t.year+1900, t.hour, t.minute, t.second<<1);
-/*
   const char path[85] = "/Users/robertalvarez/OneDrive - University of Texas at El Paso/Fall_2022/CS_4375/HW3";
   printf("Full path: %s\n", path);
 
@@ -30,26 +28,27 @@ int main(void)
     printf("token = %s\n", *token);
   }
 
-printf("sizeof(handler_t) = %zx\n", sizeof(handler_t));
-printf("sizeof(node_t) = %zx\n", sizeof(node_t));
-printf("sizeof(directory_t) = %zx\n", sizeof(directory_t));
-printf("sizeof(file_t) = %zx\n", sizeof(file_t));
-printf("sizeof(file_block_t) = %zx\n", sizeof(file_block_t));
-printf("sizeof(size_t) = %zx\n", sizeof(size_t));
-printf("sizeof(struct timespec) = %zx\n\n", sizeof(struct timespec));
-*/
+  printf("sizeof(handler_t) = %zx\n", sizeof(handler_t));
+  printf("sizeof(node_t) = %zx\n", sizeof(node_t));
+  printf("sizeof(directory_t) = %zx\n", sizeof(directory_t));
+  printf("sizeof(file_t) = %zx\n", sizeof(file_t));
+  printf("sizeof(file_block_t) = %zx\n", sizeof(file_block_t));
+  printf("sizeof(size_t) = %zx\n", sizeof(size_t));
+  printf("sizeof(struct timespec) = %zx\n\n", sizeof(struct timespec));
+
   size_t fssize = 2048;
   void *fsptr = mmap(NULL, fssize, PROT_WRITE|PROT_READ, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
 
-printf("fssize = %zx\n", fssize);
+  printf("fssize = %zx\n", fssize);
   if (fsptr != NULL) {
     handler(fsptr, fssize);
     size_t *size = malloc(sizeof(size_t));
     *size = 0x10;
     void *alloc = __malloc_impl(fsptr, NULL, size);
-printf("fsptr = %p\n", fsptr);
-printf("alloc = %p\n", alloc);
-printf("alloc-fsptr = %zx\n", (alloc-fsptr));
+
+    printf("fsptr = %p\n", fsptr);
+    printf("alloc = %p\n", alloc);
+    printf("alloc-fsptr = %zx\n", (alloc-fsptr));
 
     List *LL = (List *) get_free_memory_ptr(fsptr);
     AllocateFrom *fb = off_to_ptr(fsptr, LL->first_space);
@@ -58,9 +57,10 @@ printf("alloc-fsptr = %zx\n", (alloc-fsptr));
 
     *size = 0x10;
     void *alloc2 = __malloc_impl(fsptr, alloc, size);
-printf("fsptr = %p\n", fsptr);
-printf("alloc2 = %p\n", alloc2);
-printf("alloc2-fsptr = %zx\n", (alloc2-fsptr));
+
+    printf("fsptr = %p\n", fsptr);
+    printf("alloc2 = %p\n", alloc2);
+    printf("alloc2-fsptr = %zx\n", (alloc2-fsptr));
     __free_impl(fsptr, alloc);
 
     free(size);
@@ -68,5 +68,12 @@ printf("alloc2-fsptr = %zx\n", (alloc2-fsptr));
   }
 
   return 0;
+}
+
+void print_tokens(char **tokens)
+{
+  for ( ; *tokens; tokens++) {
+    printf("%s\n", *tokens);
+  }
 }
 
